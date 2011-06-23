d574be5dd02b4fcec9e166bfc330cd456b293be9
John Axel Eriksson <john@insane.se>
2011-06-23 18:32:22 +0200

    Updated rspec conf

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,18 +4,7 @@ $:.unshift(rrunit_path) if File.directory?(rrunit_path) && !$:.include?(rrunit_p
 require 'rrunit'
 
 RSpec.configure do |config|
-  # == Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
   config.mock_with :rspec
-  
-  ## perhaps this should be removed as well
-  ## and done in Rakefile?
   config.color_enabled = true
-  ## dont do this, do it in Rakefile instead
-  #config.formatter = 'd'
+  config.formatter = 'd'
 end
